90f3aad5a6179491feec203db9962bc117573c43
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2024-10-17 13:00:40 +0700

    [FIX]: setting env

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,6 +6,13 @@ module.exports = {
       exec_mode: "cluster",
       instances: "max",
       script: "./.output/server/index.mjs",
+      env_development: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+        NUXT_PUBLIC_API_BASE: process.env.NUXT_PUBLIC_API_BASE,
+      },
     },
   ],
 };
