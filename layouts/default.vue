8f39c75b159c63f7a22e66ebb2a00faafec4b05e
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2024-10-18 09:27:48 +0700

    [UPD]: menambahkan opsi bg canvas
    background canvas bg-gray-100

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,19 @@
+<script setup lang="ts">
+const props = defineProps({
+  bgCanvas: {
+    type: Boolean,
+  },
+});
+</script>
+
 <template>
   <MainHeader />
 
-  <UContainer class="min-h-screen">
-    <slot />
-  </UContainer>
+  <div :class="[props.bgCanvas && 'bg-gray-100 dark:bg-inherit']">
+    <UContainer class="min-h-screen">
+      <slot />
+    </UContainer>
+  </div>
 
   <MainFooter />
 </template>
