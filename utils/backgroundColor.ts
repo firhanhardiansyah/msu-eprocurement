d1be1492e5039538ba49ffd5325cd17979d40171
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2024-10-18 09:25:08 +0700

    [UPD]: menyesuaian warna sesuai dengan tema

--- utils/backgroundColor.ts
+++ utils/backgroundColor.ts
@@ -1,16 +1,16 @@
 export function getBackgroundColor(index: number): string {
   const colors = [
-    "bg-rose-500 dark:bg-rose-500",
-    "bg-orange-500 dark:bg-orange-500",
-    "bg-amber-500 dark:bg-amber-500",
-    "bg-lime-500 dark:bg-lime-500",
-    "bg-teal-500 dark:bg-teal-500",
-    "bg-sky-500 dark:bg-sky-500",
-    "bg-blue-500 dark:bg-blue-500",
-    "bg-purple-500 dark:bg-purple-500",
-    "bg-pink-500 dark:bg-pink-500",
-    "bg-green-500 dark:bg-green-500",
-    "bg-red-500 dark:bg-red-500",
+    "bg-rose-500 dark:bg-rose-700",
+    "bg-orange-500 dark:bg-orange-700",
+    "bg-amber-500 dark:bg-amber-700",
+    "bg-lime-500 dark:bg-lime-700",
+    "bg-teal-500 dark:bg-teal-700",
+    "bg-sky-500 dark:bg-sky-700",
+    "bg-blue-500 dark:bg-blue-700",
+    "bg-purple-500 dark:bg-purple-700",
+    "bg-pink-500 dark:bg-pink-700",
+    "bg-green-500 dark:bg-green-700",
+    "bg-red-500 dark:bg-red-700",
   ];
 
   return colors[index];
