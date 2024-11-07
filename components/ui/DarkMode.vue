53458623a7bc81510ff6fa97fb091c6b184e6e7e
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2024-11-07 16:38:26 +0700

    [UPD]: menambahkan tooltip pada button dark mode

--- components/ui/DarkMode.vue
+++ components/ui/DarkMode.vue
@@ -8,18 +8,28 @@ const isDark = computed({
     colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
   },
 });
+
+const darkModeTooltip = ref(toCapitalCase(`${colorMode.preference} mode`));
+
+watch(isDark, () => {
+  darkModeTooltip.value = toCapitalCase(`${colorMode.preference} mode`);
+});
 </script>
 
 <template>
   <ClientOnly>
-    <UButton
-      :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
-      color="gray"
-      aria-label="Theme"
-      @click="isDark = !isDark"
-    />
-    <template #fallback>
-      <div class="w-8 h-8" />
-    </template>
+    <UTooltip :text="darkModeTooltip" :popper="{ arrow: true }">
+      <UButton
+        :icon="
+          isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'
+        "
+        color="gray"
+        aria-label="Theme"
+        @click="isDark = !isDark"
+      />
+      <template #fallback>
+        <div class="w-8 h-8" />
+      </template>
+    </UTooltip>
   </ClientOnly>
 </template>
